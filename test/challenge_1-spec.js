931cb09d4b9f31ff09f6b04c1e2515f90475a213
Gitthere <ichi4g@gmail.com>
2014-06-12 23:31:44 -1000

    test failed ok, next work on code

--- test/challenge_1-spec.js
+++ test/challenge_1-spec.js
@@ -1,4 +1,6 @@
 var challenge_1 = require("../challenge_1");
+
+// console.log(challenge_1.sumOfAMultiple);
 var chai = require('chai')
   , expect = chai.expect
   , should = chai.should(); 
@@ -6,13 +8,12 @@ var chai = require('chai')
 describe("Sum of a Multiple", function () {
   
   it("should return '23' for n=10", function () {
-    expect(sumOfAMultiple(10)).equal(23);
+    challenge_1.sumOfAMultiple(10).should.equal(23);
   });
 
-  // it("should return '2318' for n=100", function () {
-  //   var sum = challenge_1.sumOfAMultiple(100);
-  //   expect(sum).toBe(2318);
-  // });
+  it("should return '2318' for n=100", function () {
+    challenge_1.sumOfAMultiple(100).should.equal(2318);
+  });
 
   
   
