fcf3facee142ec9e2e34786e403fcfaae2554279
Gitthere <ichi4g@gmail.com>
2014-06-12 21:44:25 -1000

    first commit deleted spec/challenge_1

--- challenge_1.js
+++ challenge_1.js
@@ -1,6 +1,12 @@
 exports.sumOfAMultiple = function(n){
   var sum = 0;
   // do your work here
-  
+  // var n = number selected;
+  // var total = 
+  // for (i = 0; i < n; i++); {
+  //   if n%3 = 0 || n%5 //check if number is multiple of 3 or 5
+    
+  // }
+
   return sum;
 };
---
+++ test/challenge_1-spec.js
@@ -0,0 +1,19 @@
+var challenge_1 = require("../challenge_1");
+var chai = require('chai')
+  , expect = chai.expect
+  , should = chai.should(); 
+
+describe("Sum of a Multiple", function () {
+  
+  it("should return '23' for n=10", function () {
+    expect(sumOfAMultiple(10)).equal(23);
+  });
+
+  // it("should return '2318' for n=100", function () {
+  //   var sum = challenge_1.sumOfAMultiple(100);
+  //   expect(sum).toBe(2318);
+  // });
+
+  
+  
+});    
