ee41911fa4c53cc4ea3143785dee367e56c97269
Gitthere <ichi4g@gmail.com>
2014-06-13 11:16:09 -1000

    working for n=10.  need to figure out how to change value for n to 100, 1000 and 10000. maybe loop through array.length

--- challenge_1.js
+++ challenge_1.js
@@ -1,12 +1,23 @@
 exports.sumOfAMultiple = function(n){
   var sum = 0;
-  // do your work here
-  // var n = ();
-  // var total = 
-  // for (i = 0; i < n; i++); {
-  //   if n%3 = 0 || n%5 //check if number is multiple of 3 or 5
-    
-  // }
 
-  return sum;
+  //do your work here
+  // var n = [10, 100, 1000, 10000];
+
+  // for (i =  0; i < n.length; i++) {
+    
+    var n = 10;
+    var total = 0;
+    var newTotal = 0;
+
+    for (i = 0; i < n; i++) {
+      //check if number is multiple of 3 or 5
+      if (i % 3 == 0 || i % 5 == 0) {
+        newTotal = newTotal + i;
+      } 
+      // console.log(newTotal);
+    }
+    return newTotal;
+    return sum;
+  //}
 };
--- test/challenge_1-spec.js
+++ test/challenge_1-spec.js
@@ -15,6 +15,19 @@ describe("Sum of a Multiple", function () {
     challenge_1.sumOfAMultiple(100).should.equal(2318);
   });
 
-  
+// when your tests pass, uncomment the next two and
+// change the expected value to your calculated result
+/*
+   it("should return 'x' for n=1000 (see challenge_1-spec.js)", function () {
+     var sum = challenge_1.sumOfAMultiple(1000);
+     expect(sum).toBe(0);
+   });
+ 
+   it("should return 'x' for n=10000 (see challenge_1-spec.js)", function () {
+    var sum = challenge_1.sumOfAMultiple(10000);
+    expect(sum).toBe(0); // you change this value to the correct sum!
+ });
+ */
+ 
   
 });    
