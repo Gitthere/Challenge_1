516a72c4c9f4a3f9bdcd4b02fafa5b263669b72a
Gitthere <ichi4g@gmail.com>
2014-06-13 14:28:22 -1000

    array loop not working. only first value logging to console, but calculationsnot working

--- challenge_1.js
+++ challenge_1.js
@@ -3,12 +3,15 @@ exports.sumOfAMultiple = function(n){
 
   //do your work here
   var n = [10, 100];//array of n values
+  
   for (i =  0; i < n.length; i++) {//loop through array of n values
     console.log(n[i]);
+    var (n[i]) = newInput;
+
     var total = 0;
     var newTotal = 0;
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < newInput; i++) {
       //check if number is multiple of 3 or 5
       if (i % 3 == 0 || i % 5 == 0) {
         newTotal = newTotal + i;
@@ -17,5 +20,6 @@ exports.sumOfAMultiple = function(n){
     }
     return newTotal;
     return sum;
+
   }
 };
