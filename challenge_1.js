62f146a214ae71aa901dbe4b572363140b4fe7f5
Gitthere <ichi4g@gmail.com>
2014-06-13 16:16:00 -1000

    n[i] still not registering in for loop, only output is the actual values in array. not calculatiing

--- challenge_1.js
+++ challenge_1.js
@@ -5,21 +5,15 @@ exports.sumOfAMultiple = function(n){
   var n = [10, 100];//array of n values
   
   for (i =  0; i < n.length; i++) {//loop through array of n values
-    console.log(n[i]);
-    var (n[i]) = newInput;
-
-    var total = 0;
-    var newTotal = 0;
-
-    for (i = 0; i < newInput; i++) {
+    // console.log(n[i]);
+    for (i = 0; i < n[i]; i++) {
       //check if number is multiple of 3 or 5
       if (i % 3 == 0 || i % 5 == 0) {
-        newTotal = newTotal + i;
-      } 
-      // console.log(newTotal);
-    }
-    return newTotal;
-    return sum;
-
-  }
-};
+        sum = sum + i;
+      };
+    };  
+  };
+  return sum;
+  };
+  
+sumOfAMultiple();
