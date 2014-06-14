68124620445af9fef92c4d379cc0aaccf7f83dc8
Gitthere <ichi4g@gmail.com>
2014-06-14 09:50:25 -1000

    fixed code. runs properly

--- challenge_1.js
+++ challenge_1.js
@@ -1,19 +1,11 @@
 exports.sumOfAMultiple = function(n){
   var sum = 0;
 
-  //do your work here
-  var n = [10, 100];//array of n values
-  
-  for (i =  0; i < n.length; i++) {//loop through array of n values
-    // console.log(n[i]);
-    for (i = 0; i < n[i]; i++) {
-      //check if number is multiple of 3 or 5
-      if (i % 3 == 0 || i % 5 == 0) {
+   for (var i =  0; i < n; i++) {
+
+     if (i % 3 == 0 || i % 5 == 0) {
         sum = sum + i;
       };
-    };  
-  };
+    };   
   return sum;
-  };
-  
-sumOfAMultiple();
+};  
